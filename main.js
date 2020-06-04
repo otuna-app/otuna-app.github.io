d73bc15f5a19979a321a27e3667bbd5d0f5f6790
otuna-app <66390020+otuna-app@users.noreply.github.com>
2020-06-04 23:20:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function verify(){
         document.getElementById("licenceinput").style.display = "none"
         document.getElementById("veriftext").style.display = "block"
         document.getElementById("veriftext").style.color = "#4dff4d"
-        document.getElementById("veriftext").innerHTML = "Licence valide"
+        document.getElementById("veriftext").innerHTML = "License valide"
         document.getElementById("verif").style.display = "none"
         document.getElementById("cydia").style.display = "flex"
         document.getElementById("site").style.display = "flex"
